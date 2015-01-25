38caa0918a57e4bf7a615a2033f8de1e6f93e3d5
William WItten <William.Witten@eagles.usm.edu>
2015-01-25 14:41:49 -0600

    added 2D DK Unity Test

---
+++ 2D DK Unity Test/Assets/Resources/Scripts/DKControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DKControllerScript : MonoBehaviour {
+
+	public float moveDistance = 5.0f;
+	bool moving = false;
+	bool facingRight = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetAxis ("Horizontal") != 0 && moving == false) {
+			moving = true;
+
+				}
+
+		else if (Input.GetAxis ("Vertical") != 0 && moving == false) {
+			moving = true;
+
+				}
+	}
+}
